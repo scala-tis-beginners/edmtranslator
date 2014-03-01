34a5ea9beb77b697327db42b511a1a6be8c8b3fa
Kazuki Negoro <s63.kazuki.n@gmail.com>
2014-03-02 00:46:31 +0900

    とりあえず動くようになった！

---
+++ src/main/scala/com/example/edmtranslator/dictionary/NotCompletelyTranslationException.scala
@@ -0,0 +1,8 @@
+package com.example.edmtranslator.dictionary
+
+/**
+ * Created by kazuki on 2014/03/01.
+ */
+case class NotCompletelyTranslationException(text: List[String]) extends IllegalStateException {
+
+}
