111fa673001109d1fe5716b2b6884675585c93a6
Kazuki Negoro <s63.kazuki.n@gmail.com>
2014-03-02 12:18:31 +0900

    リソースの close がされなくて途中までしか結果が出力できないのを修正

--- src/main/scala/com/example/edmtranslator/ui/CUI.scala
+++ src/main/scala/com/example/edmtranslator/ui/CUI.scala
@@ -6,15 +6,22 @@ import com.example.edmtranslator
 import scala.slick.driver.JdbcDriver.simple._
 import scala.xml.XML
 import scala.xml.dtd.{PublicID, DocType}
+import org.xml.sax.ext.DefaultHandler2
+import javax.xml.parsers.SAXParserFactory
+import java.nio.charset.Charset
 
 /**
  * Created by kazuki on 2014/02/21.
  */
 object CUI {
 
-  case class Config(database: String = "", input: Option[File] = None, output: Option[File] = None)
+  case class CommandOption(
+                     database: String = null,
+                     charset: Charset = Charset.forName("UTF-8"),
+                     input: Option[File] = None,
+                     output: Option[File] = None)
 
-  object parser extends OptionParser[Config]("edmtr") {
+  object optParser extends OptionParser[CommandOption]("edmtr") {
 
     head("edmtr 0.1")
 
@@ -23,11 +30,19 @@ object CUI {
       .text("jdbc connection string (support H2 only)")
       .action { (x, cfg) => cfg.copy(database = x) }
 
-
     opt[File]('o', "output").optional()
       .valueName("<file>")
       .text("output file name")
       .action { (x, cfg) => cfg.copy(output = Some(x)) }
+
+    opt[String]('c', "charset").optional()
+      .valueName("<charset>")
+      .text(
+        s"""charset to read and write (default: UTF-8)
+          |${Charset.availableCharsets().values()}
+        """.stripMargin)
+      .action { (x, cfg) => cfg.copy(charset = Charset.forName(x)) }
+      .validate( x => if (Charset.isSupported(x)) success else failure(s"$x is not supported charset."))
 
     arg[File]("<file>").optional()
       .text("input file name")
@@ -35,28 +50,64 @@ object CUI {
   }
 
   def main(args: Array[String]) = {
-    parser.parse(args, Config()) map { config =>
+    optParser.parse(args, CommandOption()) map { option =>
 
       object appConfig extends edmtranslator.Config {
-        override val database = Database.forURL(config.database)
+        override val database = Database.forURL(option.database)
       }
 
-      val input: InputStream = (config.input) match {
+      val bufferSize = 1024
+      val input = new BufferedReader(new InputStreamReader((option.input) match {
         case Some(input) => new FileInputStream(input)
         case _ => System.in
-      }
-      val output: OutputStream = (config.output) match {
+      }, option.charset), bufferSize)
+
+      val output = new BufferedWriter(new OutputStreamWriter((option.output) match {
         case Some(output) => new FileOutputStream(output)
         case _ => System.out
+      }, option.charset), bufferSize)
+
+
+      // DocType を取得できるようにハンドラを登録したパーサーを使う
+      val parser = SAXParserFactory.newInstance().newSAXParser()
+      object doctypeHandler extends DefaultHandler2 {
+        var doctype: Option[DocType] = None
+        override def startDTD(name: String, publicId: String, systemId: String) = {
+          doctype = Some(DocType(name, PublicID(publicId, systemId), Nil))
+        }
+      }
+      parser.setProperty("http://xml.org/sax/properties/lexical-handler", doctypeHandler)
+
+      val translatedXml = using(input) { i =>
+        appConfig.translator.translate(XML.withSAXParser(parser).load(i))
       }
 
-      val translatedXml = appConfig.translator.translate(XML.load(input))
-
-      val doctype = DocType("xml", PublicID("ERD", null), Nil)
-      XML.write(new OutputStreamWriter(output, "UTF-8"), translatedXml, "UTF-8", false, doctype)
+      translatedXml.map { xml =>
+        using(output) { o =>
+          XML.write(output, xml, option.charset.name(), true, doctypeHandler.doctype.orNull)
+        }
+      }
 
     } getOrElse {
+      // do nothing.
     }
   }
+
+  /**
+   * リソースを確実にクローズして結果を返す
+   *
+   * @param resource リソース
+   * @param f リソースを使う処理
+   * @return 処理結果
+   */
+  def using[A <: {def close()},B](resource:A)(f:A => B):Option[B] =
+    try{
+      Some( f(resource) )
+    }catch{
+      case e:Exception => e.printStackTrace
+        None
+    }finally{
+      if(resource != null) resource.close()
+    }
 
 }
